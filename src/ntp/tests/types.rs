ad9225dc8e67b71c1f95334d3c2dba43408729ab
plaets <plates.jsnm@gmail.com>
2021-01-16 01:35:43 +0100

    fix tests (no idea why they broke)

--- src/ntp/tests/types.rs
+++ src/ntp/tests/types.rs
@@ -6,12 +6,12 @@ fn timestamp() {
 
     assert_eq!(0x12345678, timestamp.get_seconds());
     assert_eq!(0x9abcdef0, timestamp.get_fraction());
-    assert_eq!(0x731_9abcdef0 as u64, timestamp.set_seconds(0x731).into());
+    assert_eq!(0x731_9abcdef0 as u64, u64::from(timestamp.set_seconds(0x731)));
     assert_eq!(0x731, timestamp.set_seconds(0x731).get_seconds());
 
     assert_eq!(0x9abcdef0, timestamp.set_seconds(0x731).get_fraction());
     assert_eq!(0x12345678, timestamp.set_fraction(0x52141).get_seconds());
-    assert_eq!(0x12345678_00052141 as u64, timestamp.set_fraction(0x52141).into());
+    assert_eq!(0x12345678_00052141 as u64, u64::from(timestamp.set_fraction(0x52141)));
     assert_eq!(0x52141, timestamp.set_fraction(0x52141).get_fraction());
 
 
@@ -37,12 +37,12 @@ fn short() {
 
     assert_eq!(0x1234, short.get_seconds());
     assert_eq!(0x5678, short.get_fraction());
-    assert_eq!(0x731_5678 as u32, short.set_seconds(0x731).into());
+    assert_eq!(0x731_5678 as u32, u32::from(short.set_seconds(0x731)));
     assert_eq!(0x731, short.set_seconds(0x731).get_seconds());
 
     assert_eq!(0x5678, short.set_seconds(0x731).get_fraction());
     assert_eq!(0x1234, short.set_fraction(0x5214).get_seconds());
-    assert_eq!(0x1234_5214 as u32, short.set_fraction(0x5214).into());
+    assert_eq!(0x1234_5214 as u32, u32::from(short.set_fraction(0x5214)));
     assert_eq!(0x5214, short.set_fraction(0x5214).get_fraction());
 
     assert_eq!(Short::from_duration(chrono::Duration::seconds(15)).unwrap().get_seconds(), 15);
